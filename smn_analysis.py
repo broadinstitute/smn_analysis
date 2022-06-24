0114ebccc98905e7003ec89edfc86ff830c442d0
codewithrakshya <rakshya@broadinstitute.org>
2022-06-24 10:14:33 -0400

    update

--- smn_analysis.py
+++ smn_analysis.py
@@ -1,18 +1,14 @@
 #!/usr/env python
 
 """ Read CRAM files"""
+""" The purpose of reading CRAM files - """
 import pysam
 HUMAN_G = '/Users/rakshya/SMA.2022/Homo_sapiens_assembly38.fasta'
-CRAM_FH = '/Users/rakshya/SMA.2022/MAAC057.cram'
-cram = pysam.AlignmentFile(CRAM_FH, 'rc', reference_filename=None)
-with pysam.FastxFile(HUMAN_G) as fh:
-    for entry in fh:
-        print(entry)
-#SMN1
-for read in cram.fetch(region="chr5:70924941-70953012", until_eof=True):
-    print(read)
-#SMN2
-for read in cram.fetch(region="chr5:70049523-70077595", until_eof=True):
-    print(read)
+CRAM_F = '/Users/rakshya/SMA.2022/MAAC057.cram'
+cram = pysam.AlignmentFile(CRAM_F, 'rc', reference_filename=HUMAN_G)
+SMN1 = cram.count(region="chr5:70924941-70953012")
+print("SMN1 : ", SMN1)
+SMN2 = cram.count(region="chr5:70049523-70077595")
+print("SMN2 : ", SMN2)
 cram.close()
 
