d6d5e0612497065711187b9f50080d20ee68bb06
codewithrakshya <rakshya@broadinstitute.org>
2022-06-24 11:00:30 -0400

    Update 2

--- smn_analysis.py
+++ smn_analysis.py
@@ -8,7 +8,9 @@ CRAM_F = '/Users/rakshya/SMA.2022/MAAC057.cram'
 cram = pysam.AlignmentFile(CRAM_F, 'rc', reference_filename=HUMAN_G)
 SMN1 = cram.count(region="chr5:70924941-70953012")
 print("SMN1 : ", SMN1)
+#Output - SMN1 :  7088
 SMN2 = cram.count(region="chr5:70049523-70077595")
 print("SMN2 : ", SMN2)
+#Output - SMN2 :  7438
 cram.close()
 
