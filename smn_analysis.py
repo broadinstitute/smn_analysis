7a55730f2bfc26e13dc5dd22b03ffc81dd83e739
codewithrakshya <rakshya@broadinstitute.org>
2022-06-28 15:26:47 -0400

    update.june.28

--- smn_analysis.py
+++ smn_analysis.py
@@ -2,25 +2,40 @@
 
 """Counts and prints the number of reads that overlap exon 7 of SMN1 and SMN2 in the input CRAM or BAM file. 
 """
+import os
 import pysam
 import sys
 import pandas as pd
 
-cram = pysam.AlignmentFile(sys.argv[2], 'rc', reference_filename=sys.argv[1])
-SMN1 = cram.count(region="chr5:70924941-70953012")
-SMN2 = cram.count(region="chr5:70049523-70077595")
-file_location = '/Users/rakshya/SMA.2022/smn_analysis/count.tsv'
-#parsing the cram filename
-cram_filename = (sys.argv[2].split('/')[-1]).split('.')[0]
-headerlist = ['SampleID','SMN1','SMN2','SMN1 + SMN2']
-#inserting the data in count.tsv file
-df = pd.DataFrame({'SampleID': [cram_filename], 'SMN1': [str(SMN1)], 'SMN2': [str(SMN2)], 'SMN1 + SMN2': [str(SMN1 + SMN2)]})
-df.to_csv(file_location, mode = 'a', sep='\t', index=False)
-cram.close()
+# parse arguments
+reference_fasta_path = sys.argv[1]
+cram_paths = sys.argv[2:]
+output_file_location = '/Users/rakshya/SMA.2022/smn_analysis/count.tsv'
 
-"""//Output// 
-SampleID        SMN1    SMN2    SMN1 + SMN2
-MAAC057 7088    7438    14526
-SampleID        SMN1    SMN2    SMN1 + SMN2
-MANT033 7642    9056    16698
-"""
+
+output_records = []
+for cram_path in cram_paths:
+    cram = pysam.AlignmentFile(cram_path, 'rc', reference_filename=reference_fasta_path)
+    SMN1_exon7_read_count = cram.count(region="chr5:70924941-70953012")
+    SMN2_exon7_read_count = cram.count(region="chr5:70049523-70077595")
+
+    cram_filename = os.path.basename(cram_path)
+    cram_filename = cram_filename.replace(".cram", "")
+
+    record = {
+        'SampleID': cram_filename,
+        'SMN1': SMN1_exon7_read_count,
+        'SMN2': SMN2_exon7_read_count,
+        'SMN1 + SMN2': SMN1_exon7_read_count + SMN2_exon7_read_count,
+    }
+
+    output_records.append(record)
+
+    cram.close()
+
+
+df = pd.DataFrame(output_records)
+df.to_csv(output_file_location, sep='\t', index=False)
+
+print("No of cram files: ", len(output_records), " output in", output_file_location)
+
