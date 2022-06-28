c2f33d31b7ff2940c46967b88b3528fa2017a83a
codewithrakshya <rakshya@broadinstitute.org>
2022-06-28 11:11:37 -0400

    update.june.28

--- smn_analysis.py
+++ smn_analysis.py
@@ -1,16 +1,26 @@
-#!/usr/env python
+#!/usr/env python3
 
 """Counts and prints the number of reads that overlap exon 7 of SMN1 and SMN2 in the input CRAM or BAM file. 
 """
 import pysam
-REFERENCE_FASTA_PATH = '/Users/rakshya/SMA.2022/Homo_sapiens_assembly38.fasta'
-CRAM_F = '/Users/rakshya/SMA.2022/MAAC057.cram'
-cram = pysam.AlignmentFile(CRAM_F, 'rc', reference_filename=HUMAN_G)
+import sys
+import pandas as pd
+
+cram = pysam.AlignmentFile(sys.argv[2], 'rc', reference_filename=sys.argv[1])
 SMN1 = cram.count(region="chr5:70924941-70953012")
-print("SMN1 : ", SMN1)
-#Output - SMN1 :  7088
 SMN2 = cram.count(region="chr5:70049523-70077595")
-print("SMN2 : ", SMN2)
-#Output - SMN2 :  7438
+file_location = '/Users/rakshya/SMA.2022/smn_analysis/count.tsv'
+#parsing the cram filename
+cram_filename = (sys.argv[2].split('/')[-1]).split('.')[0]
+headerlist = ['SampleID','SMN1','SMN2','SMN1 + SMN2']
+#inserting the data in count.tsv file
+df = pd.DataFrame({'SampleID': [cram_filename], 'SMN1': [str(SMN1)], 'SMN2': [str(SMN2)], 'SMN1 + SMN2': [str(SMN1 + SMN2)]})
+df.to_csv(file_location, mode = 'a', sep='\t', index=False)
 cram.close()
 
+"""//Output// 
+SampleID        SMN1    SMN2    SMN1 + SMN2
+MAAC057 7088    7438    14526
+SampleID        SMN1    SMN2    SMN1 + SMN2
+MANT033 7642    9056    16698
+"""
