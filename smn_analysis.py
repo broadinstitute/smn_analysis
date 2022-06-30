3f3cd997b908deb763462076aa788f8b647c15f1
bw2 <ben.weisburd@gmail.com>
2022-06-30 14:31:52 -0400

    Add a 'Total reads' column. Refactor script to split code into functions. Use argparse for parsing command line args.

--- smn_analysis.py
+++ smn_analysis.py
@@ -2,16 +2,13 @@
 
 """Counts and prints the number of reads that overlap exon 7 of SMN1 and SMN2 in the input CRAM or BAM file. 
 """
+import argparse
 import os
 import pysam
-import sys
 import pandas as pd
 
+
 # parse arguments
-reference_fasta_path = sys.argv[1]
-cram_paths = sys.argv[2:]
-output_file_location = '/Users/rakshya/SMA.2022/smn_analysis/add-all-exon-count.tsv'
-output_records = []
 EXON_COORDINATES = [("chr5:70925087-70925184","chr5:69345439-69345593"),
                     ("chr5:70938839-70938910","chr5:69359242-69359313"),
                     ("chr5:70941389-70941508","chr5:69361792-69361911"),
@@ -21,28 +18,100 @@ EXON_COORDINATES = [("chr5:70925087-70925184","chr5:69345439-69345593"),
                     ("chr5:70946066-70946176","chr5:69366468-69366578"),
                     ("chr5:70951941-70951994","chr5:69372348-69372401"),
                     ("chr5:70952439-70953015","chr5:69372846-69373419")]
-for cram_path in cram_paths:
-    cram = pysam.AlignmentFile(cram_path, 'rc', reference_filename=reference_fasta_path)
-    #listing exon coordinates exon 1-8
+
+EXON_LABELS = {
+    0: "1",
+    1: "2a",
+    2: "2b",
+    3: "3",
+    4: "4",
+    5: "5",
+    6: "6",
+    7: "7",
+    8: "8",
+}
+
+def parse_args():
+    """Parse and return command-line args"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-R", "--reference-fasta", required=True, help="Reference genome FASTA file path")
+    parser.add_argument("-o", "--output-tsv", default="add-all-exon-count.tsv", help="Output tsv file path")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Whether to print extra stats and output")
+    parser.add_argument("cram_paths", nargs="+", help="One or more CRAM file paths")
+    args = parser.parse_args()
+
+    # make sure the input files exist
+    for path in [args.reference_fasta] + args.cram_paths:
+        if not os.path.isfile(path):
+            parser.error(f"File not found: {path}")
+
+    return args
+
+
+def compute_total_reads(cram_path):
+    """Compute the total number of reads in the given cram file using samtools idxstat.
+
+    Args:
+        cram_path (str): local file path
+
+    Return:
+        int: The total number of reads
+    """
     cram_filename = os.path.basename(cram_path)
-    cram_filename = cram_filename.replace(".cram", "")
-    #empty dictionary
-    record = {}
-    count = 0
-    #updating the records dictionary
-    record.update({"SAMPLEID": cram_filename})
-    #looping to read exon coordinates from the list
-    for SMN1_exon_region,SMN2_exon_region in EXON_COORDINATES:
-        SMN1_exon_read_count = cram.count(region= SMN1_exon_region)
-        SMN2_exon_read_count = cram.count(region= SMN2_exon_region)
-        count+= 1
-        exon = "SMN1 + SMN2 exon " + str(count) + " reads"
-        exon_read = {exon : SMN1_exon_read_count + SMN2_exon_read_count}
-        record.update(exon_read)
+    idxstat_filename = f"{cram_filename}.idxstat.txt"
+    os.system(f"samtools idxstat {cram_path} > {idxstat_filename}")
+    idxstat_df = pd.read_table(idxstat_filename, names=[
+        "chrom", "chrom_length", "mapped_read_counts", "unmapped_read_counts",
+    ])
+    os.remove(idxstat_filename)
 
-    output_records.append(record)
-    cram.close()
-df = pd.DataFrame(output_records)
-df.to_csv(output_file_location, sep='\t', index=False)
-print("No of cram files: ", len(output_records), " output in", output_file_location)
+    total_reads = sum(idxstat_df.mapped_read_counts) + sum(idxstat_df.unmapped_read_counts)
 
+    return total_reads
+
+
+def main():
+    args = parse_args()
+
+    if args.verbose:
+        print(f"Processing {len(args.cram_paths)} CRAM files")
+
+    output_records = []
+    for cram_path in args.cram_paths:
+        cram = pysam.AlignmentFile(cram_path, 'rc', reference_filename=args.reference_fasta)
+        #listing exon coordinates exon 1-8
+        cram_filename = os.path.basename(cram_path)
+        cram_filename_prefix = cram_filename.replace(".cram", "")
+        #empty dictionary
+        record = {}
+        exon_count = 0
+        #updating the records dictionary
+        record.update({"SAMPLEID": cram_filename_prefix})
+        #looping to read exon coordinates from the list
+        for SMN1_exon_region, SMN2_exon_region in EXON_COORDINATES:
+            SMN1_exon_read_count = cram.count(region= SMN1_exon_region)
+            SMN2_exon_read_count = cram.count(region= SMN2_exon_region)
+            exon_label = EXON_LABELS[exon_count]
+            exon_count += 1
+            record.update({
+                f"SMN1 + SMN2 exon {exon_label} reads": SMN1_exon_read_count + SMN2_exon_read_count
+            })
+
+        total_reads = compute_total_reads(cram_path)
+        record.update({
+            "Total reads": total_reads,
+        })
+
+        if args.verbose:
+            print(f"{total_reads:15,d} total reads in {cram_path}")
+
+        output_records.append(record)
+        cram.close()
+
+    df = pd.DataFrame(output_records)
+    df.to_csv(args.output_tsv, sep='\t', index=False)
+    print("No of cram files: ", len(output_records), " output in", args.output_tsv)
+
+
+if __name__ == "__main__":
+    main()
